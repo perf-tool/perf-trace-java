b6aa6884f0cd7a9545b43925bac861e2a4b9f029
goflutterjava <goflutterjava@gmail.com>
2022-11-08 18:32:31 +0800

    fallback to linux eth0

--- src/main/java/io/github/perftool/trace/util/IpUtil.java
+++ src/main/java/io/github/perftool/trace/util/IpUtil.java
@@ -1,10 +1,22 @@
 package io.github.perftool.trace.util;
 
+import org.apache.commons.lang3.SystemUtils;
+
 public class IpUtil {
 
-    private static final String POD_IP = System.getenv("POD_IP");
+    private static final String POD_IP = getPodIp();
 
     private static final String FORMATTED_IP = StringTool.formatIp(POD_IP);
+
+    private static String getPodIp() {
+        String ipEnv = System.getenv("POD_IP");
+        if ("".equals(ipEnv)) {
+            if (SystemUtils.IS_OS_LINUX) {
+                return Ipv4Util.getIp("eth0");
+            }
+        }
+        return ipEnv;
+    }
 
     public static String podIp() {
         return POD_IP;
